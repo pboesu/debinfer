bcb2c7b27e26163a828f49b315818b352e69aed4
Philipp Boersch-Supan <philipp@boersch-supan.de>
2015-10-05 14:32:14 -0400

    changed inits in setinits.DEB to return a named vector

--- R/DEB_daphnia.r
+++ R/DEB_daphnia.r
@@ -16,11 +16,11 @@ DEB.daphnia<-function(t,y,p){
    gamma <- p["gamma"]
    X.h <- p["X.h"]
    vol <- p["vol"]
-   
+
    phi<-PHI(X.h, y[1]) #y[1]/(y[1]+X.h)
-   
-   F<-food(t,X.h,y[1])   
-   
+
+   F<-food(t,X.h,y[1])
+
    ## here Delta = -Delta in PTRS paper (ie, if >0 sufficient reserve
    ## to pay somatic maintenance)
    Delta <- - kap * g * J.EAm / (v * y[3]) * (( k.M * g * y[3]-y[2]*v)/ (y[2] + g))
@@ -28,12 +28,12 @@ DEB.daphnia<-function(t,y,p){
 
    ## p.c, or nearly
    p.c<-(1 - kap) * y[3]^2 * y[2] * g * J.EAm / v * ( (v + k.M* y[3]) / (y[2] + g))
-   
+
    if(Delta > 0){
      dy1 <- -J.EAm / (vol * y.EX) * y[3] * phi + F #dX
      dy2 <- v / y[3] * (phi-y[2]) #de
      dy3 <- 1/3 * ((y[2] * v - k.M * g * y[3])/(y[2] + g))#dL
-     
+
      ## Development and reproduction
      if(y[4] < M.HP){
        dy4 <-  p.c  - k.J * y[4] #dMH
@@ -64,10 +64,10 @@ DEB.daphnia<-function(t,y,p){
      ##  dy2<- dy3 <- dy4 <- dy5 <-0
      ##}
    }
-   
-   
+
+
    ##print(paste("animal is dead at time ",t,sep=""))
-   
+
    list(c(dy1,dy2,dy3,dy4,dy5))
  }
 
@@ -79,11 +79,11 @@ PHI<-function(X.h, x){
 
 
 food<-function(t, X.h, x){
-  
+
   if(t>1) f<-(1-x/(x+X.h))*(2-cos(t/10))##/2
   ##if(t%%7==0) f<-1000
   else f<-0
-  
+
   ##f<-10-2*x
   return(f)
 }
@@ -100,22 +100,36 @@ setparams.DEB<-function(J.EAm = 0.00361, y.EX = 0.5, v = 1.48,
                         k.J = 0.1, M.HP = 0.00575,
                         shape = 0.721, gamma = 630, X.h = 0.16,
                         vol = 0.1){
-  
+
     params <- c(J.EAm = J.EAm, y.EX = y.EX, v = v, k.M = k.M, g = g,
-                kap = kap, k.J = k.J*k.M, M.HP = M.HP, shape = shape, 
+                kap = kap, k.J = k.J*k.M, M.HP = M.HP, shape = shape,
                 gamma = gamma, X.h = X.h, vol = vol)
-    
+
   return(params)
 }
 
 
-## for this version of the model the initial enery density should be
-## set to the "parental" density, i.e. to food(1, X.h, X0), but for
-## the MCMC the "default is for e0==NULL, to indicate that e0 is
-## effectively also "proposed by the algorithm.
 
+
+#' Title
+#' for this version of the model the initial enery density should be
+#' set to the "parental" density, i.e. to food(1, X.h, X0), but for
+#' the MCMC the "default is for e0==NULL, to indicate that e0 is
+#' effectively also "proposed by the algorithm.
+#'
+#' @param X0
+#' @param e0
+#' @param L0
+#' @param M.H0
+#' @param M.R0
+#'
+#' @return initial states
+#' @export
+#'
+#' @examples examples
 setinits.DEB<-function(X0=0.2, e0=-Inf, L0 = 0.69, M.H0 = 0, M.R0 = 0){
-  inits<-c(X0, e0, L0, M.H0, M.R0)
+
+  inits<-c(X = X0, e = e0, L = L0, M.H = M.H0, M.R = M.R0)
   return(inits)
 }
 
@@ -145,7 +159,7 @@ add.noise<-function(data, sds, params){
   L[w]<-0
   w<-which(Negg<0)
   Negg[w]<-0
-  
+
   return(list(t=t, L=L, Negg=Negg))
 
 }
