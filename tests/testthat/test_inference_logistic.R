3a5b543329ffe9d4b9e874a766e67c93b85af2ec
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-03-15 14:12:31 -0400

    test to cover logistic example

--- tests/testthat/test_inference_logistic.R
+++ tests/testthat/test_inference_logistic.R
@@ -2,18 +2,18 @@ library(deBInfer)
 context("Testing an entire inference procedure using a simple logistic model")
 
 
-# define logistic model
-model <- function (time, y, parms) {
-  with(as.list(c(y, parms)), {
-    dN <- r * N * (1 - N / K)
-    list(dN)
-  })
-}
+
 
 test_that("Inference on simulated data returns simulation parameters.", {
   #testthat::skip_on_cran()
-
-  expect_true(existsFunction("model"))
+  # define logistic model
+  logistic_model <- function(time, y, parms) {
+    with(as.list(c(y, parms)), {
+      dN <- r * N * (1 - N / K)
+      list(dN)
+    })
+  }
+  expect_true(exists("logistic_model"))
   # set initial value for simulation
   y <- c(N = 0.1)
   # set parameter values
@@ -21,7 +21,7 @@ test_that("Inference on simulated data returns simulation parameters.", {
   # set simulation time points
   times <- seq(0, 120, 1)
   # solve ODE
-  out <- ode(y, times, model, parms, method='lsoda')
+  out <- ode(y, times, logistic_model, parms, method='lsoda')
   # sample from simulated data
   set.seed(143)
   N_obs <- as.data.frame(out[c(1,runif(35, 0, nrow(out))),]) #force include the first time-point (t=0)
@@ -32,7 +32,7 @@ test_that("Inference on simulated data returns simulation parameters.", {
 
   # define an observation model
   # NB: lognormal errors are not great really for the obs model - should be changed to sth that actually allows N to be zero instead of using epsilon correction
-  log.post.params<-function(samp, w.p, data, p, pdfs, hyper, sim.data, sds, verbose.lik=FALSE){
+  logistic_obs_model<-function(samp, w.p, data, p, pdfs, hyper, sim.data, sds, verbose.lik=FALSE){
 
     llik.N<-sum(dlnorm(data$N_noisy, meanlog=log(sim.data$N+0.000000001), sdlog=sds$N, log=TRUE))
 
@@ -50,19 +50,18 @@ test_that("Inference on simulated data returns simulation parameters.", {
 
     return( llik + lprior )
   }
-  expect_true(existsFunction("log.post.params"))
 
 
   # do inference with deBInfer
   # MCMC iterations
-  iter = 10000
+  iter = 4000
   # define burnin
   burnin = 2000
   # inference call
   mcmc_samples <- deb_mcmc(N=iter, p.start=list(r=0.5, K=5), data=N_obs, w.p=c("r", "K"), params=parms,
-                           inits=c(N=0.1), sim=model, sds=list(N=0.01), hyper=list(r=list(mean=0, sd=1),
+                           inits=c(N=0.1), sim=logistic_model, sds=list(N=0.01), hyper=list(r=list(mean=0, sd=1),
                            K=list(meanlog=1, sdlog=1)), pdfs = list(r='norm', K='lnorm'), prop.sd=c(r=0.001, K=0.1),
                            Tmax=max(N_obs$time), cnt=iter+1, burnin=200, plot=FALSE, sizestep=0.1, which = 1,
-                           data.times = N_obs$time)
+                           data.times = N_obs$time, obs.model=logistic_obs_model)
   expect_equal(colMeans(mcmc_samples$samps[burnin:iter,]),parms,tolerance = 1e-2)
 })
