0c9f499b07cc2a445f372cc79d593451d96d6e45
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-03-29 14:45:26 -0400

    added (temporary) test for dde_mcmc function

--- tests/testthat/test_inference_logistic.R
+++ tests/testthat/test_inference_logistic.R
@@ -1,10 +1,7 @@
 library(deBInfer)
 context("Testing an entire inference procedure using a simple logistic model")
 
-
-
-
-test_that("Inference on simulated data w/ estimated obs. error returns simulation parameters.", {
+test_that("Inference on simulated data w/ estimated obs. error returns simulation parameters. DEB version.", {
   #testthat::skip_on_cran()
   # define logistic model
   logistic_model <- function(time, y, parms) {
@@ -52,12 +49,68 @@ test_that("Inference on simulated data w/ estimated obs. error returns simulatio
   mcmc_samples <- deb_mcmc(N=iter, p.start=list(r=0.5, K=5, logsd.N=0.1), data=N_obs, w.p=c("r", "K", "logsd.N"), params=parms,
                            inits=c(N=0.1), sim=logistic_model, sds=list(N=0.01), hyper=list(r=list(mean=0, sd=1),
                            K=list(meanlog=1, sdlog=1), logsd.N=list(meanlog=0.1, sdlog=1)), pdfs = list(r='norm', K='lnorm', logsd.N='lnorm'), prop.sd=c(r=0.001, K=0.1, logsd.N=0.2),
-                           Tmax=max(N_obs$time), cnt=500, burnin=200, plot=TRUE, sizestep=0.1, which = 1,
+                           Tmax=max(N_obs$time), cnt=500, burnin=200, plot=FALSE, sizestep=0.1, which = 1,
                            data.times = N_obs$time, obs.model=logistic_obs_model)
   #add more tests here checking the integrity & contents of the returned data structure
   #check accuracy of estimation (threshold is 0.5% of true parameter value)
   expect_equal(unname(colMeans(mcmc_samples$samps[burnin:iter,])/parms),c(1,1),tolerance = 5e-3)
 })
+
+test_that("Inference on simulated data w/ known obs. error returns simulation parameters. Chytrid version.", {
+  #testthat::skip_on_cran()
+  # define logistic model
+  logistic_model <- function(time, y, parms) {
+    with(as.list(c(y, parms)), {
+      dN <- r * N * (1 - N / K)
+      list(dN)
+    })
+  }
+  expect_true(exists("logistic_model"))
+  # set initial value for simulation
+  y <- c(N = 0.1)
+  # set parameter values
+  parms <- c(r = 0.1, K = 10)
+  # set simulation time points
+  times <- seq(0, 120, 1)
+  # solve ODE
+  out <- ode(y, times, logistic_model, parms, method='lsoda')
+  # sample from simulated data
+  set.seed(143)
+  N_obs <- as.data.frame(out[c(1,runif(35, 0, nrow(out))),]) #force include the first time-point (t=0)
+  # add lognormal noise
+  N_obs$N_noisy <- rlnorm(nrow(N_obs), log(N_obs$N),0.01)
+  # observations must be ordered for solver to work
+  N_obs <- N_obs[order(N_obs$time),]
+
+  # define an observation model
+  # NB: lognormal errors are not great really for the obs model - should be changed to sth that actually allows N to be zero instead of using epsilon correction
+  logistic_obs_model<-function(data, sim.data, sds, samp){
+
+    llik.N<-sum(dlnorm(data$N_noisy, meanlog=log(sim.data$N+0.000000001), sdlog=sds$N, log=TRUE))
+
+    llik<-llik.N
+
+    return(llik)
+  }
+
+
+  # do inference with deBInfer
+  # MCMC iterations
+  iter = 5000
+  # define burnin
+  burnin = 2000
+  # inference call
+  mcmc_samples <- dde_mcmc(N=iter, p.start=list(r=0.5, K=5), data=N_obs, w.p=c("r", "K"), params=parms,
+                           inits=c(N=0.1), sim=logistic_model, sds=list(N=0.01), hyper=list(r=list(mean=0, sd=1),
+                                                                                            K=list(meanlog=1, sdlog=1)), pdfs = list(r='norm', K='lnorm'), prop.sd=c(r=0.001, K=0.1),
+                           Tmax=max(N_obs$time), cnt=iter+1, burnin=200, plot=FALSE, sizestep=0.1, which = 1,
+                           data.times = N_obs$time, obs.model=logistic_obs_model)
+  #add more tests here checking the integrity & contents of the returned data structure
+  #check accuracy of estimation (threshold is 0.5% of true parameter value)
+  expect_equal(unname(colMeans(mcmc_samples$samps[burnin:iter,])/parms),c(1,1),tolerance = 5e-3)
+})
+
+
 
 test_that("Inference on simulated data w/ known obs. error returns simulation parameters.", {
   #testthat::skip_on_cran()
