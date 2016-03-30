75ddf1baca7292c3deb8e1eb987cdd18f6938d8e
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-03-29 20:00:09 -0400

    started constructor function for collating individual parameters setup_debinfer

--- R/setup.mcmc.R
+++ R/setup.mcmc.R
@@ -2,23 +2,19 @@
 
 #' setup_debinfer
 #'
-#' Creates an object of class debinfer_in containing initial values, parameters, prior distributions, hyperparameters
+#' Creates an object of class debinfer_parlist containing initial values, parameters, prior distributions, hyperparameters
 #' tuning parameters etc. to set up a debinfer analysis
 #'
-#' @param var.name character vector; name of the variable
-#' @param var.type character vector; type of the variable "de.par" = parameter for the differential equation, "obs.par" = parameter of the observation model, "init" = initial condition for a state variable in the differential equation
-#' @param fixed boolean; TRUE = parameter is taken to be fixed, FALSE = parameter is to be estimated by MCMC
-#' @param value numeric; parameter value. For fixed parameters this is the value used in the analysis for free parameters this is the starting value used when setting up the MCMC chain
-#' @param block integer; number of block for jont proposal; 0 or NA mean the parameter is not to be jointly proposed
-#' @param distrib character; name of the probability distribution for the prior on the parameter. must conform to standard R naming i.e. beta ( foo = beta), binomial binom, Cauchy cauchy, chi-squared chisq, exponential exp, Fisher F f, gamma gamma, geometric geom, hypergeometric hyper, logistic logis, lognormal lnorm, negative binomial nbinom, normal norm, Poisson pois, Student t t, uniform unif, Weibull weibull, mvnorm
-#' @param hypers list of numeric vectors, hyperparameters for the prior; mean only for mvnorm.
-#' @param prop.sd numeric; tuning parameters, that is the standard deviation of the proposal distribution for each parameter
-#' @param samp.type character; type of sampler: "rw" random walk, "ind" = idenpendence
 #'
-#' @return returns an object of class debinfer_in to be fed to the mcmc function
+#' @return returns an S3 object of class debinfer_parlist to be fed to the mcmc function
 #' @export
-setup_debinfer <- function(var.name, var.type, fixed, value, block, distrib, hypers, prop.var, samp.type)
-{
+setup_debinfer <- function(...)
+{ parlist <- list(...)
+  if (!all(sapply(parlist, class) == "debinfer_par")) stop("input arguments need to be of class debinfer_par")
+  structure(parlist, class="debinfer_parlist")
+}
+
+
 #this function should combine the make.hypers and log.prior.params
   setup.mcmc.params<-function(hyper=NULL, w.p=NULL, samp.type=NULL,
                               prop.var=NULL, prop.mean=NULL, start=NULL,
@@ -159,7 +155,7 @@ setup_debinfer <- function(var.name, var.type, fixed, value, block, distrib, hyp
     return(all)
   }
 
-}
+
 
 
 # > str(mcmc.p)
