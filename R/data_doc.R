399616aff0d079e8ec06080bfc809b0439a9d3ec
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-05-26 20:18:19 -0400

    toggle warnings_as_errors; added data doc

---
+++ R/data_doc.R
@@ -0,0 +1,8 @@
+#' Spore counts of the chytrid fungus
+#'
+#' @name chytrid
+#' @docType data
+#' @author Philipp Boersch-Supan
+#' @references Voyles et al. 2012, Ecol Evol 9:2241-2249 \url{http://dx.doi.org/10.1002%2Fece3.334}
+#' @keywords data
+NULL
