2d4ee033142742b36ce75324f6b2b686fe476cb4
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-06-08 21:20:58 -0400

    finalised ode vignette

--- vignettes/chytrid_dede_example.R
+++ vignettes/chytrid_dede_example.R
@@ -113,7 +113,7 @@ iter = 500
 # inference call
 dede_rev <- de_mcmc(N = iter, data=chytrid, de.model=CSZ.dede,
                                obs.model=chytrid_obs_model, all.params=mcmc.pars,
-                               Tmax = max(chytrid$time), data.times=c(0,chytrid$time), cnt=iter %/% 10,
+                               Tmax = max(chytrid$time), data.times=c(0,chytrid$time), cnt=50,
                                plot=FALSE, sizestep=0.1, solver="dede", verbose = TRUE)
 
 ## ----message=FALSE, warning=FALSE,fig.width = 8, fig.height = 8----------
@@ -124,16 +124,40 @@ plot(dede_rev, ask=FALSE, auto.layout=FALSE)
 burnin = 50
 pairs(dede_rev, burnin = burnin, scatter=TRUE, trend=TRUE)
 post_prior_densplot(dede_rev, burnin = burnin)
-summary(dede_rev)
+
+## ------------------------------------------------------------------------
+par(mfrow=c(2,3), mgp=c(2.2, 0.8, 0))
+#define a fancy y axis label
+ylabel = expression(paste(Pr,"(", theta,"|", "Y", ")"))
+#plot the individual parameters
+post_prior_densplot(dede_rev, param="sr",xlab=expression(theta), 
+                    ylab=ylabel, show.obs=FALSE, xlim=c(0,8), 
+                    main=expression(paste("s",phantom()[{paste("r")}])))
+legend("topright", legend=c("Posterior","Prior"), lty = 1, col = c("black", "red"))
+post_prior_densplot(dede_rev, param="fs",xlab=expression(theta), 
+                    ylab=ylabel, show.obs=FALSE, xlim=c(-0.1,1.1), 
+                    main=expression(paste("f",phantom()[{paste("s")}])))
+post_prior_densplot(dede_rev, param="ds",xlab=expression(theta),
+                    ylab=ylabel, show.obs=FALSE, xlim=c(0,3), 
+                    main=expression(paste("d",phantom()[{paste("s")}])))
+post_prior_densplot(dede_rev, param="muz",xlab=expression(theta),
+                    ylab=ylabel, show.obs=FALSE, xlim=c(0,6), 
+                    main=expression(paste(mu,phantom()[{paste("Z")}])))
+post_prior_densplot(dede_rev, param="eta",xlab=expression(theta),
+                    ylab=ylabel, show.obs=FALSE, xlim=c(0,50), ylim=c(0,0.2), 
+                    main=expression(eta))
+post_prior_densplot(dede_rev, param="Tmin",xlab=expression(theta),
+                    ylab=ylabel, show.obs=FALSE, xlim=c(1.5,6.5), 
+                    main=expression(paste("T",phantom()[{paste("min")}])))
 
 ## ----post-sims-----------------------------------------------------------
-post_traj <- post_sim(dede_rev, n=20, times=0:100, burnin=burnin, output = 'all')
+post_traj <- post_sim(dede_rev, n=100, times=0:100, burnin=burnin, output = 'all', prob = 0.95)
 
 ## ----post-sims-plot, fig.width = 8, fig.height = 3-----------------------
 #median and HDI
 par(mfrow=c(1,3))
 plot(post_traj, plot.type = "medianHDI", auto.layout = FALSE)
-legend("topright", legend=c("posterior median", "95% HDI"), lty=1, col=c("red","grey"))
+legend("topright", legend=c("posterior median", "95% HDI"), lty=1, col=c("red","grey"), bty='n')
 
 
 ## ----post-sims-ensemble, fig.width = 8, fig.height = 6-------------------
--- vignettes/logistic_ode_example.R
+++ vignettes/logistic_ode_example.R
@@ -14,11 +14,13 @@ library(deBInfer)
 ## ----ode-def, message=FALSE,warning=FALSE--------------------------------
 library(deSolve)
 logistic_model <- function (time, y, parms) {
-with(as.list(c(y, parms)), {
-dN <- r * N * (1 - N / K)
-list(dN)
-})
+    with(as.list(c(y, parms)), {
+      dN <- r * N * (1 - N / K)
+      list(dN)
+      })
 }
+
+## ----pars-def------------------------------------------------------------
 y <- c(N = 0.1)
 parms <- c(r = 0.1, K = 10)
 times <- seq(0, 120, 1)
@@ -29,7 +31,8 @@ plot(out)
 
 ## ------------------------------------------------------------------------
 set.seed(143)
-N_obs <- as.data.frame(out[c(1,runif(35, 0, nrow(out))),]) #force include the first time-point (t=0)
+#force include the first time-point (t=0)
+N_obs <- as.data.frame(out[c(1,runif(35, 0, nrow(out))),]) 
 
 
 ## ------------------------------------------------------------------------
@@ -38,25 +41,17 @@ N_obs <- as.data.frame(out[c(1,runif(35, 0, nrow(out))),]) #force include the fi
   N_obs$N_noisy <- rlnorm(nrow(N_obs), log(N_obs$N),parms['sdlog.N'])
 #observations must be ordered for solver to work
 N_obs <- N_obs[order(N_obs$time),]
-
+#Plot true and noisy observations
 plot(N_obs$time, N_obs$N, ylim=c(0, max(N_obs$N,N_obs$N_noisy)))
 points(N_obs$time, N_obs$N_noisy, col="red")
-
-out_obs <- ode(y, c(0,N_obs$time), logistic_model, parms, method='lsoda')
-plot(out_obs, type="p")
-lines(out)
 
 ## ----obs-model-----------------------------------------------------------
   # the observation model
   logistic_obs_model<-function(data, sim.data, samp){
 
     llik.N<-sum(dlnorm(data$N_noisy, meanlog=log(sim.data[,"N"] + 1e-6), 
-                       sdlog=samp[['sdlog.N']], log=TRUE)
-                )
-
-    llik<-llik.N
-
-    return(llik)
+                       sdlog=samp[['sdlog.N']], log=TRUE))
+    return(llik.N)
   }
 
 
@@ -64,14 +59,14 @@ lines(out)
 library(deBInfer)
 r <- debinfer_par(name = "r", var.type = "de", fixed = FALSE,
                 value = 0.5, prior="norm", hypers=list(mean = 0, sd = 1),
-                prop.var=0.005, samp.type="rw")
+                prop.var=0.0001, samp.type="rw")
 
 K <- debinfer_par(name = "K", var.type = "de", fixed = FALSE,
                 value = 5, prior="lnorm", hypers=list(meanlog = 1, sdlog = 1),
                 prop.var=0.1, samp.type="rw")
 
 sdlog.N <- debinfer_par(name = "sdlog.N", var.type = "obs", fixed = FALSE,
-                value = 0.1, prior="lnorm", hypers=list(meanlog = 0, sdlog = 1),
+                value = 0.05, prior="lnorm", hypers=list(meanlog = 0, sdlog = 1),
                 prop.var=c(3,4), samp.type="rw-unif")
 
 ## ----inits---------------------------------------------------------------
@@ -87,7 +82,7 @@ mcmc.pars <- setup_debinfer(r, K, sdlog.N, N)
   # inference call
   mcmc_samples <- de_mcmc(N = iter, data=N_obs, de.model=logistic_model, 
                           obs.model=logistic_obs_model, all.params=mcmc.pars,
-                          Tmax = max(N_obs$time), data.times=N_obs$time, cnt=iter+1, 
+                          Tmax = max(N_obs$time), data.times=N_obs$time, cnt=500, 
                           plot=FALSE, solver="ode")
 
 
@@ -95,18 +90,27 @@ mcmc.pars <- setup_debinfer(r, K, sdlog.N, N)
 plot(mcmc_samples)
 
 ## ------------------------------------------------------------------------
-burnin = 200
+burnin = 250
 pairs(mcmc_samples, burnin = burnin, scatter=TRUE, trend=TRUE)
-post_prior_densplot(mcmc_samples, burnin = burnin)
-summary(mcmc_samples)
+
+## ------------------------------------------------------------------------
+par(mfrow=c(1,3))
+post_prior_densplot(mcmc_samples, burnin = burnin, param="r")
+abline(v=0.1, col="red", lty=2)
+post_prior_densplot(mcmc_samples, burnin = burnin, param="K")
+abline(v=10, col="red", lty=2)
+post_prior_densplot(mcmc_samples, burnin = burnin, param="sdlog.N")
+abline(v=0.05, col="red", lty=2)
 
 ## ----post-sims-----------------------------------------------------------
-post_traj <- post_sim(mcmc_samples, n=200, times=0:100, burnin=burnin, output = 'all')
+post_traj <- post_sim(mcmc_samples, n=500, times=0:100, burnin=burnin, output = 'all', prob = 0.95)
 
 ## ----post-sims-plot------------------------------------------------------
 #median and HDI
-plot(post_traj, plot.type = "medianHDI")
-legend("topleft", legend=c("posterior median", "95% HDI"), lty=1, col=c("red","grey"))
+plot(post_traj, plot.type = "medianHDI", lty = c(2,1), lwd=3, col=c("red","grey20"),
+     panel.first=lines(out, col="darkblue", lwd=2))
+legend("topleft", legend=c("posterior median", "95% HDI", "true model"),
+       lty=c(2,1,1), lwd=c(3,2,2), col=c("red","grey20","darkblue"), bty='n')
 
 
 ## ----post-sims-ensemble--------------------------------------------------
