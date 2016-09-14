ae32e85c85c908f5d906e37bb6eeefc4aebbd1f3
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-09-14 18:53:58 -0400

    tidied up prior eval benchmarking

--- sandbox/prior_evaluation_test.R
+++ sandbox/prior_evaluation_test.R
@@ -19,27 +19,33 @@ get_logd_prior <- function(x, pdf, ...){ #ellipsis and get more efficient, but h
   }
 }
 
+
+match_logd_prior <- function(x, pdf, ...){ #ellipsis and get more efficient, but how to pass in named hypers in the place of ellipsis?
+  if (pdf == 'mvnorm'){
+    stop('multivariate priors not yet implemented')
+  } else {
+    g <- match.fun(paste("d", pdf, sep = ""))
+    lp <- g(x, log=TRUE, ...)
+    return(lp)
+  }
+}
 ##what about assembling call from pdf & hypers at sart of mcmc, then evaluating call?
 
 
-system.time(
-  for (i in 1:100000) logd_prior(x=1:25, pdf='norm', hypers=c(mean=1,sd=1))
+microbenchmark::microbenchmark(
+  do.call = logd_prior(x=1, pdf='norm', hypers=c(mean=1,sd=1)),
+  get = get_logd_prior(x=1, pdf='norm', mean=1, sd=1),
+  match.fun = match_logd_prior(x=1, pdf='norm', mean=1, sd=1),
+  times=1e5
 )
 
-system.time(
-  for (i in 1:100000) get_logd_prior(x=1:25, pdf='norm', mean=1, sd=1)
-)
-
-system.time(
-  for (i in 1:100000) do.call('get_logd_prior', list(x=1:25, pdf='norm', mean=2, sd=1))
-)
-
-system.time(
-  for (i in 1:100000) if ('norm' == 'mvnorm') { stop('multivariate priors not yet implemented')
-  } else { dnorm(x=1:25, mean=1,sd=1, log=TRUE)}
-)
 
 #testing truncated prior
 require(truncdist)
 get_logd_prior(x=2, pdf="trunc", spec="norm", mean=1, sd=1, a=1, b=3)
+
+logd_prior(x=2, pdf="trunc2", hypers = list(spec="norm", mean=1, sd=1, a=1, b=3))
 #works, but might break logic?
+
+dtrunc2 <- truncdist::dtrunc
+
